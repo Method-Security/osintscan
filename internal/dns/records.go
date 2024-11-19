5d88533d323e22fb062b0ab0ac1ba640047a15a3
Steven Rand <steven@method.security>
2024-11-19 14:33:58 -0500

    use the codegen structs and delete explicit ones

--- internal/dns/records.go
+++ internal/dns/records.go
@@ -5,77 +5,54 @@ import (
 
 	"github.com/miekg/dns"
 	"github.com/projectdiscovery/dnsx/libs/dnsx"
+
+	osintscan "github.com/Method-Security/osintscan/generated/go"
 )
 
-// Record represents a single DNS record.
-type Record struct {
-	Name  string `json:"name" yaml:"name"`
-	TTL   uint32 `json:"ttl" yaml:"ttl"`
-	Type  string `json:"type" yaml:"type"`
-	Value string `json:"value" yaml:"value"`
-}
-
-// Records represents all of the DNS records for a given domain.
-type Records struct {
-	A     []Record `json:"a" yaml:"a"`
-	AAAA  []Record `json:"aaaa" yaml:"aaaa"`
-	MX    []Record `json:"mx" yaml:"mx"`
-	TXT   []Record `json:"txt" yaml:"txt"`
-	NS    []Record `json:"ns" yaml:"ns"`
-	CNAME []Record `json:"cname" yaml:"cname"`
-}
-
-// RecordsReport represents the report of all DNS records for a given domain including all non-fatal errors that occurred.
-type RecordsReport struct {
-	Domain     string   `json:"domain" yaml:"domain"`
-	DNSRecords Records  `json:"dns_records" yaml:"dns_records"`
-	Errors     []string `json:"errors" yaml:"errors"`
-}
-
-func getDNSRecords(domain string) (Records, error) {
+func getDNSRecords(domain string) (osintscan.DnsRecords, error) {
 	options := dnsx.DefaultOptions
 	var questionTypes []uint16 = []uint16{dns.TypeA, dns.TypeAAAA, dns.TypeMX, dns.TypeTXT, dns.TypeNS, dns.TypeCNAME}
 	options.QuestionTypes = questionTypes
 	options.MaxRetries = 5
 	client, err := dnsx.New(options)
 	if err != nil {
-		return Records{}, err
+		return osintscan.DnsRecords{}, err
 	}
 
-	dnsRecords := Records{}
+	dnsRecords := osintscan.DnsRecords{}
 
 	results, err := client.QueryMultiple(domain)
 	if err != nil {
-		return Records{}, err
+		return osintscan.DnsRecords{}, err
 	}
 
-	populateRecords := func(records []string, recordType string) []Record {
-		var dnsRecordsSlice []Record
+	populateRecords := func(records []string, recordType string) []*osintscan.DnsRecord {
+		var dnsRecordsSlice []*osintscan.DnsRecord
 		for _, record := range records {
-			dnsRecord := Record{
+			dnsRecord := osintscan.DnsRecord{
 				Name:  domain,
-				TTL:   results.TTL, // This assumes a common TTL for all records; adjust if needed
+				Ttl:   int(results.TTL), // This assumes a common TTL for all records; adjust if needed
 				Type:  recordType,
 				Value: record,
 			}
-			dnsRecordsSlice = append(dnsRecordsSlice, dnsRecord)
+			dnsRecordsSlice = append(dnsRecordsSlice, &dnsRecord)
 		}
 		return dnsRecordsSlice
 	}
 
 	dnsRecords.A = populateRecords(results.A, "A")
-	dnsRecords.AAAA = populateRecords(results.AAAA, "AAAA")
-	dnsRecords.CNAME = populateRecords(results.CNAME, "CNAME")
-	dnsRecords.MX = populateRecords(results.MX, "MX")
-	dnsRecords.NS = populateRecords(results.NS, "NS")
-	dnsRecords.TXT = populateRecords(results.TXT, "TXT")
+	dnsRecords.Aaaa = populateRecords(results.AAAA, "AAAA")
+	dnsRecords.Cname = populateRecords(results.CNAME, "CNAME")
+	dnsRecords.Mx = populateRecords(results.MX, "MX")
+	dnsRecords.Ns = populateRecords(results.NS, "NS")
+	dnsRecords.Txt = populateRecords(results.TXT, "TXT")
 
 	return dnsRecords, nil
 }
 
 // GetDomainDNSRecords queries DNS for all records for a given domain. It returns a RecordsReport struct containing
 // all records that were and any non-fatal errors that occurred.
-func GetDomainDNSRecords(ctx context.Context, domain string) (RecordsReport, error) {
+func GetDomainDNSRecords(ctx context.Context, domain string) (osintscan.DnsRecordsReport, error) {
 	errors := []string{}
 
 	// 1. Get all the DNS records
@@ -85,9 +62,9 @@ func GetDomainDNSRecords(ctx context.Context, domain string) (RecordsReport, err
 	}
 
 	// 2. Create report and write to file
-	report := RecordsReport{
+	report := osintscan.DnsRecordsReport{
 		Domain:     domain,
-		DNSRecords: dnsRecords,
+		DnsRecords: &dnsRecords,
 		Errors:     errors,
 	}
 	return report, nil
