231326c9920e27fa739068b3447ecac25c5c7ae7
Steven Rand <steven@method.security>
2024-11-19 15:13:25 -0500

    refactor a bit to be less redundant

--- internal/dns/records.go
+++ internal/dns/records.go
@@ -2,6 +2,7 @@ package dns
 
 import (
 	"context"
+	"slices"
 
 	"github.com/miekg/dns"
 	"github.com/projectdiscovery/dnsx/libs/dnsx"
@@ -23,28 +24,8 @@ func populateRecords(domain string, ttl int, records []string, recordType string
 	return dnsRecordsSlice
 }
 
-// DMARC records are stored in a subdomain of the target domain, and therefore must be fetched separately
-func getDmarcRecords(domain string) (osintscan.DnsRecords, error) {
+func getDNSRecords(domain string, questionTypes []uint16) (osintscan.DnsRecords, error) {
 	options := dnsx.DefaultOptions
-	options.QuestionTypes = []uint16{dns.TypeTXT}
-	options.MaxRetries = 5
-	client, err := dnsx.New(options)
-	if err != nil {
-		return osintscan.DnsRecords{}, err
-	}
-	dmarcDomain := "_dmarc." + domain // The DMARC record is always in the _dmarc subdomain (RFC-7489)
-	results, err := client.QueryOne(dmarcDomain)
-	if err != nil {
-		return osintscan.DnsRecords{}, err
-	}
-	dmarcRecords := osintscan.DnsRecords{}
-	dmarcRecords.Txt = populateRecords(dmarcDomain, int(results.TTL), results.TXT, "TXT")
-	return dmarcRecords, nil
-}
-
-func getDNSRecords(domain string) (osintscan.DnsRecords, error) {
-	options := dnsx.DefaultOptions
-	var questionTypes []uint16 = []uint16{dns.TypeA, dns.TypeAAAA, dns.TypeMX, dns.TypeTXT, dns.TypeNS, dns.TypeCNAME}
 	options.QuestionTypes = questionTypes
 	options.MaxRetries = 5
 	client, err := dnsx.New(options)
@@ -59,12 +40,24 @@ func getDNSRecords(domain string) (osintscan.DnsRecords, error) {
 		return osintscan.DnsRecords{}, err
 	}
 
-	dnsRecords.A = populateRecords(domain, int(results.TTL), results.A, "A")
-	dnsRecords.Aaaa = populateRecords(domain, int(results.TTL), results.AAAA, "AAAA")
-	dnsRecords.Cname = populateRecords(domain, int(results.TTL), results.CNAME, "CNAME")
-	dnsRecords.Mx = populateRecords(domain, int(results.TTL), results.MX, "MX")
-	dnsRecords.Ns = populateRecords(domain, int(results.TTL), results.NS, "NS")
-	dnsRecords.Txt = populateRecords(domain, int(results.TTL), results.TXT, "TXT")
+	if slices.Contains(questionTypes, dns.TypeA) {
+		dnsRecords.A = populateRecords(domain, int(results.TTL), results.A, "A")
+	}
+	if slices.Contains(questionTypes, dns.TypeAAAA) {
+		dnsRecords.Aaaa = populateRecords(domain, int(results.TTL), results.AAAA, "AAAA")
+	}
+	if slices.Contains(questionTypes, dns.TypeCNAME) {
+		dnsRecords.Cname = populateRecords(domain, int(results.TTL), results.CNAME, "CNAME")
+	}
+	if slices.Contains(questionTypes, dns.TypeMX) {
+		dnsRecords.Mx = populateRecords(domain, int(results.TTL), results.MX, "MX")
+	}
+	if slices.Contains(questionTypes, dns.TypeNS) {
+		dnsRecords.Ns = populateRecords(domain, int(results.TTL), results.NS, "NS")
+	}
+	if slices.Contains(questionTypes, dns.TypeTXT) {
+		dnsRecords.Txt = populateRecords(domain, int(results.TTL), results.TXT, "TXT")
+	}
 
 	return dnsRecords, nil
 }
@@ -74,18 +67,20 @@ func getDNSRecords(domain string) (osintscan.DnsRecords, error) {
 func GetDomainDNSRecords(ctx context.Context, domain string) (osintscan.DnsRecordsReport, error) {
 	errors := []string{}
 
-	// 1. Get all the DNS records
-	dnsRecords, err := getDNSRecords(domain)
+	// Get all the DNS records
+	var questionTypes []uint16 = []uint16{dns.TypeA, dns.TypeAAAA, dns.TypeMX, dns.TypeTXT, dns.TypeNS, dns.TypeCNAME}
+	dnsRecords, err := getDNSRecords(domain, questionTypes)
 	if err != nil {
 		errors = append(errors, err.Error())
 	}
 
-	dmarcRecords, err := getDmarcRecords(domain)
+	// The DMARC record is always in the _dmarc subdomain (RFC-7489) and therefore must be fetched separately
+	dmarcRecords, err := getDNSRecords("_dmarc."+domain, []uint16{dns.TypeTXT})
 	if err != nil {
 		errors = append(errors, err.Error())
 	}
 
-	// 2. Create report and write to file
+	// Create report and write to file
 	report := osintscan.DnsRecordsReport{
 		Domain:          domain,
 		DnsRecords:      &dnsRecords,
