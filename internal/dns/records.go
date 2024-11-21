301c3b7fe645c0760a81d2eaf35f0ed9973cc033
steven-rand <steven@method.security>
2024-11-21 09:16:04 -0500

    Sr/add dkim and dmarc to dns records (#12)
    * add fern def for dns records and run codegen
    
    * use the codegen structs and delete explicit ones
    
    * query dmarc record
    
    * refactor a bit to be less redundant
    
    * get dkim as well
    
    * newline
    
    * smaller diff
    
    * unnecessarily specifying default value
    
    * also return the DKIM and DMARC subdomain names for easier processing

--- internal/dns/records.go
+++ internal/dns/records.go
@@ -2,94 +2,113 @@ package dns
 
 import (
 	"context"
+	"slices"
 
 	"github.com/miekg/dns"
 	"github.com/projectdiscovery/dnsx/libs/dnsx"
+
+	osintscan "github.com/Method-Security/osintscan/generated/go"
 )
 
-// Record represents a single DNS record.
-type Record struct {
-	Name  string `json:"name" yaml:"name"`
-	TTL   uint32 `json:"ttl" yaml:"ttl"`
-	Type  string `json:"type" yaml:"type"`
-	Value string `json:"value" yaml:"value"`
-}
-
-// Records represents all of the DNS records for a given domain.
-type Records struct {
-	A     []Record `json:"a" yaml:"a"`
-	AAAA  []Record `json:"aaaa" yaml:"aaaa"`
-	MX    []Record `json:"mx" yaml:"mx"`
-	TXT   []Record `json:"txt" yaml:"txt"`
-	NS    []Record `json:"ns" yaml:"ns"`
-	CNAME []Record `json:"cname" yaml:"cname"`
-}
-
-// RecordsReport represents the report of all DNS records for a given domain including all non-fatal errors that occurred.
-type RecordsReport struct {
-	Domain     string   `json:"domain" yaml:"domain"`
-	DNSRecords Records  `json:"dns_records" yaml:"dns_records"`
-	Errors     []string `json:"errors" yaml:"errors"`
-}
-
-func getDNSRecords(domain string) (Records, error) {
+func getDNSRecords(domain string, questionTypes []uint16) (osintscan.DnsRecords, error) {
 	options := dnsx.DefaultOptions
-	var questionTypes []uint16 = []uint16{dns.TypeA, dns.TypeAAAA, dns.TypeMX, dns.TypeTXT, dns.TypeNS, dns.TypeCNAME}
 	options.QuestionTypes = questionTypes
-	options.MaxRetries = 5
 	client, err := dnsx.New(options)
 	if err != nil {
-		return Records{}, err
+		return osintscan.DnsRecords{}, err
 	}
 
-	dnsRecords := Records{}
+	dnsRecords := osintscan.DnsRecords{}
 
 	results, err := client.QueryMultiple(domain)
 	if err != nil {
-		return Records{}, err
+		return osintscan.DnsRecords{}, err
 	}
 
-	populateRecords := func(records []string, recordType string) []Record {
-		var dnsRecordsSlice []Record
+	populateRecords := func(records []string, recordType string) []*osintscan.DnsRecord {
+		var dnsRecordsSlice []*osintscan.DnsRecord
 		for _, record := range records {
-			dnsRecord := Record{
+			dnsRecord := osintscan.DnsRecord{
 				Name:  domain,
-				TTL:   results.TTL, // This assumes a common TTL for all records; adjust if needed
+				Ttl:   int(results.TTL), // This assumes a common TTL for all records; adjust if needed
 				Type:  recordType,
 				Value: record,
 			}
-			dnsRecordsSlice = append(dnsRecordsSlice, dnsRecord)
+			dnsRecordsSlice = append(dnsRecordsSlice, &dnsRecord)
 		}
 		return dnsRecordsSlice
 	}
 
-	dnsRecords.A = populateRecords(results.A, "A")
-	dnsRecords.AAAA = populateRecords(results.AAAA, "AAAA")
-	dnsRecords.CNAME = populateRecords(results.CNAME, "CNAME")
-	dnsRecords.MX = populateRecords(results.MX, "MX")
-	dnsRecords.NS = populateRecords(results.NS, "NS")
-	dnsRecords.TXT = populateRecords(results.TXT, "TXT")
+	if slices.Contains(questionTypes, dns.TypeA) {
+		dnsRecords.A = populateRecords(results.A, "A")
+	}
+	if slices.Contains(questionTypes, dns.TypeAAAA) {
+		dnsRecords.Aaaa = populateRecords(results.AAAA, "AAAA")
+	}
+	if slices.Contains(questionTypes, dns.TypeCNAME) {
+		dnsRecords.Cname = populateRecords(results.CNAME, "CNAME")
+	}
+	if slices.Contains(questionTypes, dns.TypeMX) {
+		dnsRecords.Mx = populateRecords(results.MX, "MX")
+	}
+	if slices.Contains(questionTypes, dns.TypeNS) {
+		dnsRecords.Ns = populateRecords(results.NS, "NS")
+	}
+	if slices.Contains(questionTypes, dns.TypeTXT) {
+		dnsRecords.Txt = populateRecords(results.TXT, "TXT")
+	}
 
 	return dnsRecords, nil
 }
 
 // GetDomainDNSRecords queries DNS for all records for a given domain. It returns a RecordsReport struct containing
 // all records that were and any non-fatal errors that occurred.
-func GetDomainDNSRecords(ctx context.Context, domain string) (RecordsReport, error) {
+func GetDomainDNSRecords(ctx context.Context, domain string) (osintscan.DnsRecordsReport, error) {
 	errors := []string{}
 
-	// 1. Get all the DNS records
-	dnsRecords, err := getDNSRecords(domain)
+	// Get all the DNS records
+	var questionTypes []uint16 = []uint16{dns.TypeA, dns.TypeAAAA, dns.TypeMX, dns.TypeTXT, dns.TypeNS, dns.TypeCNAME}
+	dnsRecords, err := getDNSRecords(domain, questionTypes)
 	if err != nil {
 		errors = append(errors, err.Error())
 	}
 
-	// 2. Create report and write to file
-	report := RecordsReport{
-		Domain:     domain,
-		DNSRecords: dnsRecords,
-		Errors:     errors,
+	// The DMARC record is always in the _dmarc subdomain (RFC-7489) and therefore must be fetched separately
+	dmarcRecords, err := getDNSRecords("_dmarc."+domain, []uint16{dns.TypeTXT})
+	if err != nil {
+		errors = append(errors, err.Error())
 	}
+
+	// The DKIM record is always in the _domainkey subdomain (RFC-6376) and therefore must be fetched separately.
+	// To complicate matters, the _domainkey subdomain itself includes a subdomain named after a selector which we
+	// don't know in advance, so we need to check each common selector that we're aware of.
+	dkimRecords := osintscan.DnsRecords{}
+	var selectors []string = []string{"default", "selector1", "selector2", "google", "amazonses", "microsoft"}
+	for _, selector := range selectors {
+		dkimRecordForSelector, err := getDNSRecords(selector+"._domainkey."+domain, []uint16{dns.TypeTXT})
+		if err != nil {
+			errors = append(errors, err.Error())
+		}
+		dkimRecords.Txt = append(dkimRecords.Txt, dkimRecordForSelector.Txt...)
+	}
+
+	// Create report and write to file
+	report := osintscan.DnsRecordsReport{
+		Domain:          domain,
+		DnsRecords:      &dnsRecords,
+		DmarcDnsRecords: &dmarcRecords,
+		DkimDnsRecords:  &dkimRecords,
+		Errors:          errors,
+	}
+
+	// Set the domain for the DMARC and DKIM records if they exist
+	if len(dmarcRecords.Txt) > 0 {
+		report.DmarcDomain = &dmarcRecords.Txt[0].Name
+	}
+	if len(dkimRecords.Txt) > 0 {
+		report.DkimDomain = &dkimRecords.Txt[0].Name
+	}
+
 	return report, nil
 
 }
