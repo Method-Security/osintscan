5a643ea5fd0a8889624bd3aabd3f0095a9bcccc2
Philip Ayoub <philip@method.security>
2025-08-20 07:32:06 -0400

    move function

--- internal/discover/dns/subdomain/active.go
+++ internal/discover/dns/subdomain/active.go
@@ -2,9 +2,7 @@ package subdomain
 
 import (
 	"context"
-	"crypto/rand"
 	"fmt"
-	"math/big"
 	"net"
 	"sync"
 	"sync/atomic"
@@ -195,22 +193,6 @@ func generatePermutations(validSubdomains []string, subdomainList []string) []st
 		}
 	}
 	return results
-}
-
-// generateRandomSubdomain generates a high-entropy subdomain with only letters (16 characters).
-func generateRandomSubdomain(domain string) (string, error) {
-	letterBytes := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-	randomString := make([]byte, 16)
-	for i := range randomString {
-		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(letterBytes))))
-		if err != nil {
-			return "", err
-		}
-		randomString[i] = letterBytes[n.Int64()]
-	}
-
-	return fmt.Sprintf("%s.%s", string(randomString), domain), nil
 }
 
 // GetDiscoverDNSSubdomainActiveWordlistPath returns the file path for a given wordlist size
--- internal/discover/dns/subdomain/helpers.go
+++ internal/discover/dns/subdomain/helpers.go
@@ -3,6 +3,9 @@ package subdomain
 import (
 	// standard
 	"context"
+	"crypto/rand"
+	"fmt"
+	"math/big"
 	"net"
 )
 
@@ -24,6 +27,22 @@ func detectWildcardDNS(ctx context.Context, domain string, resolver *net.Resolve
 		return &wildcardDomain, nil
 	}
 
-	// If the error is NXDOMAIN or SERVFAIL, wildcard is NOT present
+	// If the error, domain didnt resolve so wildcard is NOT present
 	return nil, nil
+}
+
+// generateRandomSubdomain generates a high-entropy subdomain with only letters (16 characters).
+func generateRandomSubdomain(domain string) (string, error) {
+	letterBytes := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	randomString := make([]byte, 16)
+	for i := range randomString {
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(letterBytes))))
+		if err != nil {
+			return "", err
+		}
+		randomString[i] = letterBytes[n.Int64()]
+	}
+
+	return fmt.Sprintf("%s.%s", string(randomString), domain), nil
 }
